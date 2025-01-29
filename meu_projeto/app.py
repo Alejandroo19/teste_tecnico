147ba1943f454d2e6db13abff99ede43b5b8386e
Alejandroo19 <alealejandrolidio@gmail.com>
2025-01-29 19:37:07 -0300

    flask

--- meu_projeto/app.py
+++ meu_projeto/app.py
@@ -181,4 +181,5 @@ def index():
 
 # ✅ Executa o servidor Flask
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False) 
